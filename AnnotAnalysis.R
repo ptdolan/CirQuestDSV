12e05d5e60b02079d8c83ed0dec6a3116d1fc852
ptdolan <ptdolan@stanford.edu>
2016-07-16 11:25:44 -0700

    Plotting improvements
    Plot on square root axis

--- AnnotAnalysis.R
+++ AnnotAnalysis.R
@@ -27,7 +27,7 @@ for (q20file in list.files(inputDir,pattern = 'annot.txt',full.names = T)){ #Rea
   q20<-read.delim(q20file,head=T)
   q20$name<-as.factor(name)
   q20$HsN<-rep(unlist(by(q20$count,q20$ntpos,function(X){sum(X/sum(X)*log2(X/sum(X)))}[[1]])),each=4)
-  filter = q20[q20$wtNT!=q20$mutNT,] #filter out WT
+  filter = q20[q20$wtNT!=q20$mutNT&q20$count>3,] #filter out WT and low counts(by binom)
   compq20<-rbind(compq20,filter)
   
   assign(name,read.delim(q20file,sep="\t"))  #?
@@ -36,13 +36,17 @@ for (q20file in list.files(inputDir,pattern = 'annot.txt',full.names = T)){ #Rea
   #pdf(paste(inputDir,name,"_ManhattanPlot.pdf"),width=7,height=5)
   Qf<-ggplot(filter)
   
-  PP<-Qf+geom_point(aes(ntpos,freq,cex = freq,color=synNonsyn),alpha=.6)+
-    ylim(1e-6,1)+geom_text(cex=2,position = position_nudge(y=.15),data=filter[filter$freq>FILTER,],aes(ntpos,freq,label=paste(wtRes,resPos,muRes)))+
-    scale_y_log10()+ggtitle(name)+scale_size(trans = "sqrt",range=c(.5,2.5))+
-    theme_classic()+ylab("Frequency")+xlab("Genome Position")+scale_color_brewer(palette = "Set1")+facet_grid(ORF~.)
+  PP<-Qf+geom_hline(aes(yintercept = FILTER),lty=3,color="grey")+geom_point(aes(ntpos,freq,cex = freq,color=synNonsyn),alpha=0.4)+
+    ylim(1e-6,1)+geom_text(cex=2,position = position_nudge(y=.01),data=filter[filter$freq>FILTER,],aes(ntpos,freq,label=paste(wtRes,resPos,muRes)))+
+    ggtitle(name)+scale_size(trans = "sqrt",range=c(.5,2.5))+
+    theme_classic()+ylab("Frequency")+xlab("Genome Position")+
+    scale_color_brewer(palette = "Set1")+facet_grid(ORF~.)
   
-  ggsave(filename = paste(inputDir,name,"_ManhattanPlot.pdf"),
-         device = 'pdf',PP)
+  ggsave(filename = paste(inputDir,name,"_log10_ManhattanPlot.pdf"),
+         device = 'pdf',PP+scale_y_log10())
+
+  ggsave(filename = paste(inputDir,name,"_sqrt_ManhattanPlot.pdf"),
+         device = 'pdf',PP+scale_y_sqrt(breaks=c(0.0001,.001,.01,.05,.1,.2,.4,.6,.8,1)))
   
   write.csv(file=paste(inputDir,"/",name,"_0.005CO.csv",sep=""),filter[order(filter$freq,decreasing = T),])
   
